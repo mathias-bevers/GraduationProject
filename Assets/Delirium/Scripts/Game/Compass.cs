d6b4d9135135c50f4baaf9a8fe7beb7adb0e9236
amber eissink <ambereissink@gmail.com>
2021-03-25 17:09:06 +0100

    Prefab Collider Fixes, Compass and MissionPointer

---
+++ Assets/Delirium/Scripts/Game/Compass.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    Vector3 NorthDirection;
+    public Transform player;
+    public Quaternion MissionDirection;
+
+    public Transform MissionPlace;
+    public Transform CompassImage;
+    public Transform MissionPointer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeCompass();
+        ChangeMissionPointer();
+    }
+    public void ChangeCompass()
+    {
+        NorthDirection.z = player.eulerAngles.y;
+        CompassImage.localEulerAngles = NorthDirection;
+    }
+
+    public void MissionTrigger()
+    {
+
+    }
+
+    public void ChangeMissionPointer()
+    {
+        Vector3 dir = transform.position - MissionPlace.position;
+
+        MissionDirection = Quaternion.LookRotation(dir);
+
+        MissionDirection.z = -MissionDirection.y;
+        MissionDirection.x = 0;
+        MissionDirection.y = 0;
+
+        MissionPointer.localRotation = MissionDirection * Quaternion.Euler(NorthDirection);
+    }
+}
