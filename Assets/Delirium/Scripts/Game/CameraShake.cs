7d9465219e262747241f63d319de76c0858033c5
amber eissink <ambereissink@gmail.com>
2021-03-26 13:18:36 +0100

    Edit of Chase Music en CameraShake wat nog opgeroepen moet worden

---
+++ Assets/Delirium/Scripts/Game/CameraShake.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class CameraShake : MonoBehaviour
+{
+    
+    public PostProcessProfile volume;
+    public float power = 0.5f;
+    public float duration = 1.0f;
+    public Transform camera;
+    public float slowDownAmount = 1.0f;
+    public bool shouldShake = false;
+
+    Vector3 startPosition;
+    float initialDuration;
+
+    private Vignette vignetteEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main.transform;
+        startPosition = camera.localPosition;
+        initialDuration = duration;
+        volume.TryGetSettings(out vignetteEffect);
+        vignetteEffect.intensity.value = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldShake)
+        {
+            if (duration > 0)
+            {
+                //vignetteEffect.intensity.value = Mathf.Lerp(vignetteEffect.intensity.value, 15, .05f * Time.deltaTime);
+                camera.localPosition = startPosition + Random.insideUnitSphere * power;
+                duration -= Time.deltaTime * slowDownAmount;
+            }
+            else
+            {
+                shouldShake = false;
+                duration = initialDuration;
+                camera.localPosition = startPosition;
+            }
+        }
+    }
+}
